01b3da563651beab1adb0dc65439c3daa28093d5
byarbrough <6315292+byarbrough@users.noreply.github.com>
2020-03-23 15:32:12 -0400

    added instance

--- infrastrucutre/server/main.tf
+++ infrastrucutre/server/main.tf
@@ -2,7 +2,21 @@
 Module for wireguard server to serve as a relay
 ************************************************/
 
+# ssh key for accessing the server
 resource "aws_key_pair" "wg_server_key_pair" {
   key_name_prefix = "wg-server-"
   public_key      = var.public_key_pair
+}
+
+# the server itself
+resource "aws_instance" "wg_server" {
+	ami = var.wg_server_ami
+	instance_type = var.wg_server_size
+	key_name = wg_server_key_pair.key_name
+	monitoring = true
+	associate_public_ip_adress = true
+	tags = {
+		Name = "Wireguard Relay Server"
+		Project = "wg-relay"
+	}
 }
--- infrastrucutre/server/vars.tf
+++ infrastrucutre/server/vars.tf
@@ -1,3 +1,12 @@
 variable public_key_pair {
   type = string
 }
+
+variable wg_server_ami {
+	type = string
+}
+
+variable wg_server_size {
+	type = string
+	default = "t3.small"
+}
