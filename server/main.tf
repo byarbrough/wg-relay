81c7336a7038a0fb657d402b95ce63ecb684d32e
byarbrough <6315292+byarbrough@users.noreply.github.com>
2020-03-25 08:49:36 -0400

    proper ubuntu image

--- server/main.tf
+++ server/main.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "wg_server_key_pair" {
 
 # the server itself
 resource "aws_instance" "wg_server" {
-  ami                         = "Minimal Ubuntu 19.04 - Disco"
+  ami                         = "ami-0682ababa71304573"  # ubuntu-eoan-19.10-amd64-minimal-20191
   instance_type               = var.wg_server_size
   key_name                    = aws_key_pair.wg_server_key_pair.key_name
   monitoring                  = true
