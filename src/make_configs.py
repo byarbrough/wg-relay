a25aeb844164ad45f19a95a5004abd64e950ce71
byarbrough <5711546-byarbrough@users.noreply.gitlab.com>
2020-04-21 15:08:22 -0400

    use library to check ip

--- src/make_configs.py
+++ src/make_configs.py
@@ -2,27 +2,17 @@
 Create the wireguard server and configuration files
 """
 from pathlib import Path
-from re import search
 from wg import keys
+from ipaddress import ip_address
 import sys
 
 
-def is_ip(ip):
-    """ Use regular expression to verify that argument is an IP"""
-    ip_regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\.(
-        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\.(
-        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\.(
-        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)'''
-
-    return search(ip_regex, ip)
-
-
 # verify arguments
-if len(sys.argv) != 2:
-    raise TypeError('Eaxctly 1 argument required: server public IP address')
-
-if not is_ip(sys.argv[1]):
-    raise ValueError('Expected valid ip address, got', sys.argv[1])
+try:
+    ip = ip_address(sys.argv[1])
+except ValueError:
+    print('IP address is invalid: %s' % sys.argv[1])
+    print('Usage : %s  ip' % sys.argv[0])
 
 
 # gloabl variables
