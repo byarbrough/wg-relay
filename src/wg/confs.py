7276683c2052c6165158f2790d821362c97bfb44
byarbrough <5711546-byarbrough@users.noreply.gitlab.com>
2020-04-22 12:11:01 -0400

    added relay class

--- src/wg/confs.py
+++ src/wg/confs.py
@@ -2,6 +2,9 @@
 Module for generating WireGuard interface configuration files
 """
 from ipaddress import IPv4Network
+from ipaddress import IPv4Address
+from ipaddress import ip_network
+from .keys import get_pubkey
 
 
 def get_cidr(network):
@@ -14,7 +17,7 @@ def get_cidr(network):
     return sum(bin(int(x)).count('1') for x in netmask.split('.'))
 
 
-def new_relay(relay_ip, cidr, relay_listen_port, relay_private_key):
+def new_relay(relay):
     """Return conf for relay"""
 
     new_relay_fmt = \
@@ -23,7 +26,74 @@ Address = {RELAY_IP}/{CIDR}
 ListenPort = {RELAY_LISTEN_PORT}
 PrivateKey = {RELAY_PRIVATE_KEY}
 '''
-    return new_relay_fmt.format(RELAY_IP=relay_ip,
-                                CIDR=cidr,
-                                RELAY_LISTEN_PORT=relay_listen_port,
-                                RELAY_PRIVATE_KEY=relay_private_key)
+    return new_relay_fmt.format(RELAY_IP=relay.wg_ip,
+                                CIDR=relay.wg_cidr,
+                                RELAY_LISTEN_PORT=relay.listen_port,
+                                RELAY_PRIVATE_KEY=relay.wg_private_key)
+
+
+def new_peer(peer_ip, peer_priv_key, relay):
+    """Create a new peer configuration"""
+
+    # layout for text
+    relay_peer_fmt = \
+        '''[Peer]
+PublicKey = {PEER_PUB_KEY}
+AllowedIPs = {PEER_IP}/32
+
+'''
+
+    new_peer_fmt = \
+        '''[Interface]
+Address = {PEER_IP}/{CIDR}
+PrivateKey = {PEER_PRIVATE_KEY}
+
+[Peer]
+PublicKey = {RELAY_PUBLIC_KEY}
+AllowedIPs = {SUBNET}
+Endpoint = {RELAY_PUBLIC_IP}:{RELAY_LISTEN_PORT}
+PersistentKeepalive=23
+'''
+
+    # format the new peer
+    new_peer = new_peer_fmt.format(PEER_IP=peer_ip,
+                                   CIDR=relay.wg_cidr,
+                                   PEER_PRIVATE_KEY=peer_priv_key,
+                                   RELAY_PUBLIC_KEY=relay.wg_public_key,
+                                   SUBNET=relay.wg_subnet,
+                                   RELAY_PUBLIC_IP=relay.public_ip,
+                                   RELAY_LISTEN_PORT=relay.listen_port)
+
+    # format the addition to relay configuration
+    relay_peer = relay_peer_fmt.format(PEER_PUB_KEY=get_pubkey(peer_priv_key),
+                                       PEER_IP=peer_ip)
+
+    return (new_peer, relay_peer)
+
+
+class WgRelay(object):
+    """WireGuard Relay Object"""
+
+    wg_ip = IPv4Address
+    wg_subnet = IPv4Network
+    wg_cidr = 0
+    public_ip = ''
+    listen_port = 0
+    wg_private_key = ''
+    wg_public_key = ''
+
+    def __init__(self, wg_subnet, public_ip, listen_port, wg_private_key):
+        """Initialize the class"""
+        self.wg_ip = next(wg_subnet.hosts())  # first host on subnet
+        self.wg_subnet = ip_network(wg_subnet)
+        self.wg_cidr = get_cidr(wg_subnet)
+        self.public_ip = public_ip
+        self.listen_port = listen_port
+        self.wg_private_key = wg_private_key
+        self.wg_public_key = get_pubkey(wg_private_key)
+
+
+def init_wgrelay(wg_subnet, public_ip, listen_port, wg_private_key):
+    """Create a relay"""
+    wgr = WgRelay(wg_subnet, public_ip, listen_port, wg_private_key)
+    return wgr
--- tests/wg/confs_test.py
+++ tests/wg/confs_test.py
@@ -30,16 +30,35 @@ def test_get_cidr():
 def test_new_relay():
     """Test conf.new_relay()"""
 
-    relay_ip = ip_address('10.37.0.1')
-    cidr = 24
-    relay_listen_port = 51820
+    net = ip_network('172.20.0.0/16')
+    public_ip = ip_address('65.73.48.2')
+    listen_port = 7532
     relay_private_key = 'ICCdVYBjzJn+flYk3v8+o+LUkfeinO/hLK9G6AIg7UU='
+
+    relay = confs.init_wgrelay(net, public_ip, listen_port, relay_private_key)
+
     expected = \
         '''[Interface]
-Address = 10.37.0.1/24
-ListenPort = 51820
+Address = 172.20.0.1/16
+ListenPort = 7532
 PrivateKey = ICCdVYBjzJn+flYk3v8+o+LUkfeinO/hLK9G6AIg7UU=
 '''
-    assert confs.new_relay(relay_ip, cidr,
-                           relay_listen_port,
-                           relay_private_key) == expected
+    assert confs.new_relay(relay) == expected
+
+
+def test_relay_class():
+    """Test the creation of a confs Relay object"""
+    net = ip_network('10.13.0.0/24')
+    public_ip = ip_address('1.2.3.4')
+    listen_port = 51820
+    wg_private_key = 'yNEoiTM24mAaJ6jyRbUCfScazt1lJAhTUb5wdR1R1Vc='
+
+    wgr = confs.init_wgrelay(net, public_ip, listen_port, wg_private_key)
+
+    assert wgr.wg_ip == ip_address('10.13.0.1')
+    assert wgr.wg_subnet == net
+    assert wgr.wg_cidr == 24
+    assert wgr.public_ip == public_ip
+    assert wgr.listen_port == listen_port
+    assert wgr.wg_private_key == wg_private_key
+    assert wgr.wg_public_key == 'jCWSoP7TYPcgTKS2GJ3P9c1YKDcEGFwLiS0yo/HRJmI='
