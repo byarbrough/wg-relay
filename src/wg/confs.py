a282b0108f2b28c35810393217d680a188e21d99
byarbrough <5711546-byarbrough@users.noreply.gitlab.com>
2020-04-22 10:50:35 -0400

    added new_relay()

--- src/wg/confs.py
+++ src/wg/confs.py
@@ -5,6 +5,25 @@ from ipaddress import IPv4Network
 
 
 def get_cidr(network):
-	"""Helper function to convert netmask to CIDR"""
-	netmask = str(network.netmask)
-	return sum(bin(int(x)).count('1') for x in netmask.split('.'))
+    """Helper function to convert netmask to CIDR"""
+
+    # convert to IPv4Network if not already
+    network = IPv4Network(network)
+    netmask = str(network.netmask)
+    # add up the bits
+    return sum(bin(int(x)).count('1') for x in netmask.split('.'))
+
+
+def new_relay(relay_ip, cidr, relay_listen_port, relay_private_key):
+    """Return conf for relay"""
+
+    new_relay_fmt = \
+        '''[Interface]
+Address = {RELAY_IP}/{CIDR}
+ListenPort = {RELAY_LISTEN_PORT}
+PrivateKey = {RELAY_PRIVATE_KEY}
+'''
+    return new_relay_fmt.format(RELAY_IP=relay_ip,
+                                CIDR=cidr,
+                                RELAY_LISTEN_PORT=relay_listen_port,
+                                RELAY_PRIVATE_KEY=relay_private_key)
--- tests/wg/confs_test.py
+++ tests/wg/confs_test.py
@@ -2,11 +2,44 @@
 Test the wg.confs module
 """
 from ipaddress import ip_network
+from ipaddress import ip_address
 from src.wg import confs
+import pytest
 
 
 def test_get_cidr():
-    """ Test confs.get_cidr()"""
+    """Test confs.get_cidr()"""
 
     net = ip_network('192.168.0.0/30')
     assert confs.get_cidr(net) == 30
+
+    net = ip_network('10.13.0.0/24')
+    assert confs.get_cidr(net) == 24
+
+    net = ip_network('172.14.0.0/16')
+    assert confs.get_cidr(net) == 16
+
+    net = '192.168.1.0/24'
+    assert confs.get_cidr(net) == 24
+
+    # bad IP address
+    with pytest.raises(ValueError):
+        confs.get_cidr('192.168.0.0/48')
+
+
+def test_new_relay():
+    """Test conf.new_relay()"""
+
+    relay_ip = ip_address('10.37.0.1')
+    cidr = 24
+    relay_listen_port = 51820
+    relay_private_key = 'ICCdVYBjzJn+flYk3v8+o+LUkfeinO/hLK9G6AIg7UU='
+    expected = \
+        '''[Interface]
+Address = 10.37.0.1/24
+ListenPort = 51820
+PrivateKey = ICCdVYBjzJn+flYk3v8+o+LUkfeinO/hLK9G6AIg7UU=
+'''
+    assert confs.new_relay(relay_ip, cidr,
+                           relay_listen_port,
+                           relay_private_key) == expected
